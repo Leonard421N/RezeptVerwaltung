af9bd6e2fd9b31c90fb28ecf105c148d87ebb83c
Nickels, Leonard <leonard.nickels@student.fhws.de>
2019-04-15 23:04:31 +0200

    added switch case

--- app/src/main/java/wizardofba/rezeptverwaltung/MainActivity.java
+++ app/src/main/java/wizardofba/rezeptverwaltung/MainActivity.java
@@ -11,25 +11,11 @@ import wizardofba.rezeptverwaltung.Manage.ItemAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int STATE_RECEPIS = 0;
+    public static final int STATE_INGREDIENTS = 1;
+
     private RecyclerView recyclerView;
     private ItemAdapter itemAdapter;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.recepis:
-                    return true;
-                case R.id.ingredients:
-                    return true;
-                case R.id.settings:
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +28,35 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.recepis:
+                    fillRecyclerView(STATE_RECEPIS);
+                    return true;
+                case R.id.ingredients:
+                    fillRecyclerView(STATE_INGREDIENTS);
+                    return true;
+                case R.id.settings:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void fillRecyclerView(int state) {
+
+        switch(state) {
+            case STATE_RECEPIS:
+                break;
+            case STATE_INGREDIENTS:
+                break;
+        }
     }
 
 }
