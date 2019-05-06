47f1eaad7df71bf28a96706ec23078deef9de87a
Nickels, Leonard <leonard.nickels@student.fhws.de>
2019-05-07 00:13:07 +0200

    added taking photos still not finished

--- app/src/main/java/wizardofba/rezeptverwaltung/AddItemActivity.java
+++ app/src/main/java/wizardofba/rezeptverwaltung/AddItemActivity.java
@@ -1,8 +1,14 @@
 package wizardofba.rezeptverwaltung;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +16,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,10 +32,13 @@ public class AddItemActivity extends AppCompatActivity {
     private int CURRENT_STATE = 0;
     public static final int NEW_STATE = 0;
     public static final int UPDATE_STATE = 1;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     EditText name;
     FloatingActionButton saveButton;
     private RecyclerView recyclerView;
+    private ImageView imageView;
     private static IngredientAdapter ingredientAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
@@ -47,6 +57,7 @@ public class AddItemActivity extends AppCompatActivity {
 
         saveButton = (FloatingActionButton) findViewById(R.id.add_item_button_save);
         name = (EditText) findViewById(R.id.add_item_name);
+        imageView = (ImageView) findViewById(R.id.add_item_picture);
 
         ingredientAdapter = new IngredientAdapter(IngredientAdapter.CUSTOM_STATE);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_add_item);
@@ -76,6 +87,30 @@ public class AddItemActivity extends AppCompatActivity {
                     name.clearFocus();
                 }
                 return false;
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open camera or gallery
+                if (ContextCompat.checkSelfPermission(AddItemActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(AddItemActivity.this,
+                            Manifest.permission.CAMERA)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(AddItemActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                MY_CAMERA_PERMISSION_CODE);
+                    }
+                }
+                else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
             }
         });
 
@@ -117,4 +152,40 @@ public class AddItemActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_CAMERA_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }
+    }
+
 }
+
